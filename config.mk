47405585f947310f1d828b465699b8d9bdf6753d
Sergey Kulanov <skulanov@mirantis.com>
2015-10-19 11:53:26 +0300

    Migrate stackforge to openstack namespace

--- config.mk
+++ config.mk
@@ -25,26 +25,26 @@ NAILGUN_COMMIT?=master
 OSTF_COMMIT?=master
 PYTHON_FUELCLIENT_COMMIT?=master
 
-ASTUTE_REPO?=https://github.com/stackforge/fuel-astute.git
-CREATEMIRROR_REPO?=https://github.com/stackforge/fuel-mirror.git
-FUEL_AGENT_REPO?=https://github.com/stackforge/fuel-agent.git
-FUEL_MAIN_REPO?=https://github.com/stackforge/fuel-main.git
-FUEL_NAILGUN_AGENT_REPO?=https://github.com/stackforge/fuel-nailgun-agent.git
-FUELLIB_REPO?=https://github.com/stackforge/fuel-library.git
-NAILGUN_REPO?=https://github.com/stackforge/fuel-web.git
-OSTF_REPO?=https://github.com/stackforge/fuel-ostf.git
-PYTHON_FUELCLIENT_REPO?=https://github.com/stackforge/python-fuelclient.git
+ASTUTE_REPO?=https://github.com/openstack/fuel-astute.git
+CREATEMIRROR_REPO?=https://github.com/openstack/fuel-mirror.git
+FUEL_AGENT_REPO?=https://github.com/openstack/fuel-agent.git
+FUEL_MAIN_REPO?=https://github.com/openstack/fuel-main.git
+FUEL_NAILGUN_AGENT_REPO?=https://github.com/openstack/fuel-nailgun-agent.git
+FUELLIB_REPO?=https://github.com/openstack/fuel-library.git
+NAILGUN_REPO?=https://github.com/openstack/fuel-web.git
+OSTF_REPO?=https://github.com/openstack/fuel-ostf.git
+PYTHON_FUELCLIENT_REPO?=https://github.com/openstack/python-fuelclient.git
 
 # Gerrit URLs and commits
-ASTUTE_GERRIT_URL?=https://review.openstack.org/stackforge/fuel-astute
-CREATEMIRROR_GERRIT_URL?=https://review.openstack.org/stackforge/fuel-mirror
-FUEL_AGENT_GERRIT_URL?=https://review.openstack.org/stackforge/fuel-agent
-FUEL_MAIN_GERRIT_URL?=https://review.openstack.org/stackforge/fuel-main
-FUEL_NAILGUN_AGENT_GERRIT_URL?=https://review.openstack.org/stackforge/fuel-nailgun-agent
-FUELLIB_GERRIT_URL?=https://review.openstack.org/stackforge/fuel-library
-NAILGUN_GERRIT_URL?=https://review.openstack.org/stackforge/fuel-web
-OSTF_GERRIT_URL?=https://review.openstack.org/stackforge/fuel-ostf
-PYTHON_FUELCLIENT_GERRIT_URL?=https://review.openstack.org/stackforge/python-fuelclient
+ASTUTE_GERRIT_URL?=https://review.openstack.org/openstack/fuel-astute
+CREATEMIRROR_GERRIT_URL?=https://review.openstack.org/openstack/fuel-mirror
+FUEL_AGENT_GERRIT_URL?=https://review.openstack.org/openstack/fuel-agent
+FUEL_MAIN_GERRIT_URL?=https://review.openstack.org/openstack/fuel-main
+FUEL_NAILGUN_AGENT_GERRIT_URL?=https://review.openstack.org/openstack/fuel-nailgun-agent
+FUELLIB_GERRIT_URL?=https://review.openstack.org/openstack/fuel-library
+NAILGUN_GERRIT_URL?=https://review.openstack.org/openstack/fuel-web
+OSTF_GERRIT_URL?=https://review.openstack.org/openstack/fuel-ostf
+PYTHON_FUELCLIENT_GERRIT_URL?=https://review.openstack.org/openstack/python-fuelclient
 
 ASTUTE_GERRIT_COMMIT?=none
 CREATEMIRROR_GERRIT_COMMIT?=none
